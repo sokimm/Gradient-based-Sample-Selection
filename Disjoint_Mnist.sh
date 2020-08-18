24318873f29395407d2cacc551418a0da5ca8872
sokimm <sy9604@kaist.ac.kr>
2020-08-18 17:38:35 +0900

    seed, results revision

--- Disjoint_Mnist.sh
+++ Disjoint_Mnist.sh
@@ -2,6 +2,7 @@
 results="./results/Disjoint_Mnist_5/"
 MNIST_Split="--n_layers 2 --n_hiddens 100 --data_path ./data/ --save_path $results --batch_size 10 --log_every 100 --samples_per_task 1000 --data_file mnist_split.pt --cuda no  --tasks_to_preserve 4"
 MY_PYTHON="python"
+mkdir results
 mkdir $results
 cd data/
 
@@ -44,22 +45,16 @@ do
 	echo "***********************FSS_Clust***********************"
 	$MY_PYTHON main.py $MNIST_Split --model FSS_Clust --lr 0.05  --n_memories 100 --n_sampled_memories 300 --n_constraints 10 --memory_strength 0  --n_iter 3 --slack 0 --change_th 0.0 --repass 0 --eval_memory yes --normalize  no --seed $seed --subselect 0
 
-
 	echo "***********************Rand***********************"
 	$MY_PYTHON main.py $MNIST_Split --model rehearse_per_batch_rand --lr 0.05  --n_memories 100 --n_sampled_memories 300 --n_constraints 10 --memory_strength 0  --n_iter 3 --slack 0 --change_th 0.0 --repass 0 --eval_memory yes --normalize  no --seed $seed --subselect 0
 
 	echo "***********************GSS_Greedy***********************"
-	$MY_PYTHON main.py $MNIST_Split --model GSS_Greedy --lr 0.05  --n_memories 10 --n_sampled_memories 300 --n_constraints 10 --memory_strength 10  --n_iter 3  --slack 0 --change_th 0.0 --repass 0 --eval_memory yes --normalize  no --seed $seed --subselect 1 --age 0 --memory_strength 10 
+	$MY_PYTHON main.py $MNIST_Split --model GSS_Greedy --lr 0.05  --n_memories 10 --n_sampled_memories 300 --n_constraints 10 --memory_strength 10  --n_iter 3  --slack 0 --change_th 0.0 --repass 0 --eval_memory yes --normalize  no --seed $seed --subselect 1 --age 0 --memory_strength 10
 
 	echo "***********************GSS_IQP***********************"
 	$MY_PYTHON main.py $MNIST_Split --model GSS_IQP_Rehearse --lr 0.05  --n_memories 100 --n_sampled_memories 300 --n_constraints 10 --memory_strength 0  --n_iter 3 --slack 0 --change_th 0.0 --repass 0 --eval_memory yes --normalize  no --seed $seed --subselect 0 --age 0 --memory_strength 0 --change_th 0.0
 
-	((seed++))
+	seed=$((seed+1))
 done
 
-$MY_PYTHON stats_calculater.py  $results 5
-
-
-
-
-
+#$MY_PYTHON stats_calculater.py  $results 5
